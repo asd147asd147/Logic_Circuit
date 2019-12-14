2f32bf4ba11d5fbbfba6a47a2419af0e389588fb
asd147asd147 <asd147asd147@naver.com>
2019-12-14 15:50:33 +0900

    Version 3.1

--- input_lcd.v
+++ input_lcd.v
@@ -21,37 +21,43 @@ always @ (posedge RESETN or posedge CLK or posedge ENABLE)
 begin
   if (RESETN)
     STATE = DELAY;
-  else if(CLK && ENABLE)
-    begin
-      case (STATE)
-        DELAY :
-          if(CNT == 70) STATE = LINE1;
-        LINE1 :
-          if(CNT == 20) STATE = LINE2;
-        LINE2 :
-          if(CNT == 20) STATE = LINE1;
-      endcase
-    end
+  else if(CLK)
+	begin
+		if(ENABLE)
+		 begin
+			case (STATE)
+			  DELAY :
+				 if(CNT == 70) STATE = LINE1;
+			  LINE1 :
+				 if(CNT == 20) STATE = LINE2;
+			  LINE2 :
+				 if(CNT == 20) STATE = LINE1;
+			endcase
+		 end
+	 end
 end
 
 always @ (posedge RESETN or posedge CLK or posedge ENABLE)
 begin
   if(RESETN)
     CNT = 0;
-  else if(CLK && ENABLE)
-    begin
-      case (STATE)
-          DELAY :
-            if (CNT == 70) CNT = 0;
-            else CNT = CNT + 1;
-          LINE1 :
-            if (CNT >= 20) CNT = 0;
-            else CNT = CNT + 1;
-          LINE2 :
-            if (CNT >= 20) CNT = 0;
-            else CNT = CNT + 1;
-      endcase
-    end
+  else if(CLK)
+	begin
+		if(ENABLE)
+		 begin
+			case (STATE)
+				 DELAY :
+					if (CNT == 70) CNT = 0;
+					else CNT = CNT + 1;
+				 LINE1 :
+					if (CNT >= 20) CNT = 0;
+					else CNT = CNT + 1;
+				 LINE2 :
+					if (CNT >= 20) CNT = 0;
+					else CNT = CNT + 1;
+			endcase
+		 end
+	end
 end
 
 
@@ -61,52 +67,55 @@ begin
     begin
       OUTPUT_DATA = 8'b00100000;
     end
-  else if(CLK && ENABLE)
-    begin
-      case (STATE)
-        LINE1 :
-          begin
-            case (CNT)
-              0 :
-                begin
-                  OUTPUT_DATA = 8'b01000011;
-                end
-              1 :
-                begin
-                  OUTPUT_DATA = 8'b01101111;
-                end
-              2 :
-                begin
-                  OUTPUT_DATA = 8'b01101110;
-                end
-              default :
-                begin
-                  OUTPUT_DATA = 8'b01100101;
-                end
-            endcase
-          end
-        LINE2 :
-          begin
-            case (CNT)
-              0 :
-                begin
-                  OUTPUT_DATA = 8'b01000011;
-                end
-              1 :
-                begin
-                  OUTPUT_DATA = 8'b01101111;
-                end
-              2 :
-                begin
-                  OUTPUT_DATA = 8'b01101110;
-                end
-              default :
-                begin
-                  OUTPUT_DATA = 8'b01100101;
-                end
-            endcase
-          end
-        endcase
-    end
+  else if(CLK)
+	begin
+		if(ENABLE)
+		 begin
+			case (STATE)
+			  LINE1 :
+				 begin
+					case (CNT)
+					  0 :
+						 begin
+							OUTPUT_DATA = 8'b01000011;
+						 end
+					  1 :
+						 begin
+							OUTPUT_DATA = 8'b01101111;
+						 end
+					  2 :
+						 begin
+							OUTPUT_DATA = 8'b01101110;
+						 end
+					  default :
+						 begin
+							OUTPUT_DATA = 8'b01100101;
+						 end
+					endcase
+				 end
+			  LINE2 :
+				 begin
+					case (CNT)
+					  0 :
+						 begin
+							OUTPUT_DATA = 8'b01000011;
+						 end
+					  1 :
+						 begin
+							OUTPUT_DATA = 8'b01101111;
+						 end
+					  2 :
+						 begin
+							OUTPUT_DATA = 8'b01101110;
+						 end
+					  default :
+						 begin
+							OUTPUT_DATA = 8'b01100101;
+						 end
+					endcase
+				 end
+			  endcase
+		 end
+	end
 end
 endmodule
